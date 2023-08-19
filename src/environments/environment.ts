8128e977033bc2a7eea9e4b08dbe9ff84e7a047f
Weps <weaponz326@gmail.com>
2023-08-19 00:17:46 +0000

    deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,20 @@
 export const environment = {
     production: true,
     firebase: {
-        apiKey: "AIzaSyCSHmgoAYPLCqay6W5VMrjJwU2EgCnj5Rk",
-        authDomain: "murgerhanhub.firebaseapp.com",
-        projectId: "murgerhanhub",
-        storageBucket: "murgerhanhub.appspot.com",
-        messagingSenderId: "314027253691",
-        appId: "1:314027253691:web:ad5a05580368cd41142e9e",
-        measurementId: "G-2HWLEX27E5"
+        // apiKey: "AIzaSyCSHmgoAYPLCqay6W5VMrjJwU2EgCnj5Rk",
+        // authDomain: "murgerhanhub.firebaseapp.com",
+        // projectId: "murgerhanhub",
+        // storageBucket: "murgerhanhub.appspot.com",
+        // messagingSenderId: "314027253691",
+        // appId: "1:314027253691:web:ad5a05580368cd41142e9e",
+        // measurementId: "G-2HWLEX27E5"
 
-        // apiKey: "AIzaSyBT10IX6Y8ZJroL6jI75raWprbC01ATXY0",
-        // authDomain: "weapons-7d089.firebaseapp.com",
-        // projectId: "weapons-7d089",
-        // storageBucket: "weapons-7d089.appspot.com",
-        // messagingSenderId: "337928193877",
-        // appId: "1:337928193877:web:aa64d1dce4fe4bb76591dc",
-        // measurementId: "G-LLQLNRM53Q"
+        apiKey: "AIzaSyBT10IX6Y8ZJroL6jI75raWprbC01ATXY0",
+        authDomain: "weapons-7d089.firebaseapp.com",
+        projectId: "weapons-7d089",
+        storageBucket: "weapons-7d089.appspot.com",
+        messagingSenderId: "337928193877",
+        appId: "1:337928193877:web:aa64d1dce4fe4bb76591dc",
+        measurementId: "G-LLQLNRM53Q"
     }
 };
